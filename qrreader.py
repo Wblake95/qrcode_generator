f609908badb17f8164eb5c1069b9719722a1b50a
Wesley Blake <wesleyblake95@gmail.com>
2022-06-25 16:12:12 -0700

    Update
    Update to make it read

--- qrreader.py
+++ qrreader.py
@@ -1,3 +1,11 @@
 #! /usr/bin/env python3
 
+from pyzbar.pyzbar import decode
+from PIL import Image
 
+pth = input("What is the path to the qrcode? ")
+img = Image.open(pth)
+
+result = decode(img)
+
+print(result)
