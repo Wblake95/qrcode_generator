02a305411c177cd4e97bf54467ee2d9faabc12b5
Wesley Blake <wesleyblake95@gmail.com>
2022-06-25 15:41:14 -0700

    uploaded update
    Update made to resources and inputs

--- qrmaker.py
+++ qrmaker.py
@@ -1,32 +1,33 @@
 #! /usr/bin/env python3
 
-# Tutorial:
-# Freecodecamp: https://www.freecodecamp.org/news/python-projects-for-beginners/#qr-code-encoder-decoder-python-project
-# https://www.youtube.com/watch?v=SqvVm3QiQVk&t=3192s
 # Resources:
 # https://pypi.org/project/qrcode/
 # https://www.thepythoncode.com/article/generate-read-qr-code-python
+# Freecodecamp: https://www.freecodecamp.org/news/python-projects-for-beginners/#qr-code-encoder-decoder-python-project
+# https://www.youtube.com/watch?v=SqvVm3QiQVk&t=3192s
 
 import qrcode
 
-data = input("What is your URL?" )
-# Link in qrcode
+url = input("What is your URL?" )
 version = int(input("What version of the code would you like? "))
-boxsize = int(input("What would you like your box size to be? "))
+box_size = int(input("What would you like your box size to be? "))
 border = int(input("What would you like your boarder size to be? "))
-fillcolor = input("What would you like your fill color to be? ").lower()
-backcolor = input("What would you like your back color to be? ").lower()
+fill_color = input("What would you like your fill color to be? ").lower()
+back_color = input("What would you like your back color to be? ").lower()
+file_name = input("What would you like your file to be named? ")
+file_name = file_name + ".png"
+# Options for the qrcode
 
-qr = qrcode.QRCode(version = version, box_size = boxsize, border = border)
+qcode = qrcode.QRCode(version = version, box_size = box_size, border = border)
 # Look of qrcode
-qr.add_data(data)
+qcode.add_data(url)
 # Adding data to qrcode
-qr.make(fit=True)
+qcode.make(fit=True)
 # Make the alterations fit
 
-img = qr.make_image(fill_color = fillcolor, back_color = backcolor)
+img = qcode.make_image(fill_color = fill_color, back_color = back_color)
 # The colors of the qrcode
 
-img.save("/home/shadoughie/Pictures/qrcode_make_python.png")
+img.save(f"/home/shadoughie/Pictures/{file_name}")
 # Make sure this file isn't called qrcode.py
 # For some reason this will return an error
